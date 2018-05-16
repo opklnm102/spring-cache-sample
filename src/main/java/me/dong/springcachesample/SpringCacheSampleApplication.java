8133232de511cdb43ef07e33d3652224471cc796
Dong Hee Kim <opklnm102@gmail.com>
2018-05-16 21:38:31 +0900

    initial commit

---
+++ src/main/java/me/dong/springcachesample/SpringCacheSampleApplication.java
@@ -0,0 +1,12 @@
+package me.dong.springcachesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCacheSampleApplication.class, args);
+    }
+}
