646bd4e4befaaa6c65ffbf260cdd88fbd6e06d17
Dong Hee Kim <opklnm102@gmail.com>
2018-05-19 20:02:36 +0900

    Add: spring boot1 redis cache sample module 추가

---
+++ spring-boot1-redis-cache/src/main/java/me/dong/springboot1rediscache/SpringBoot1RedisCacheApplication.java
@@ -0,0 +1,12 @@
+package me.dong.springboot1rediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1RedisCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot1RedisCacheApplication.class, args);
+    }
+}
---
+++ spring-boot1-redis-cache/src/test/java/me/dong/springboot1rediscache/SpringBoot1RedisCacheApplicationTests.java
@@ -0,0 +1,16 @@
+package me.dong.springboot1rediscache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot1RedisCacheApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
